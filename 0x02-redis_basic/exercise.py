22d815f8c357b5fe0d6480b1a0e2582bf7f35e9d
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-01-25 01:02:26 +0100

    all done

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""Exercise for Redis"""
+
+import redis
+import uuid
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator that takes a single method Callable argument and returns a Callable"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function"""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwds)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs for a particular function"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+
+        output = str(method(self, *args, **kwds))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+
+        return output
+
+    return wrapper
+
+
+def replay(method: Callable):
+    """Display the history of calls of a particular function"""
+
+    r = redis.Redis()
+    method_name = method.__qualname__
+    inputs = r.lrange(method_name + ":inputs", 0, -1)
+    outputs = r.lrange(method_name + ":outputs", 0, -1)
+
+    print("{} was called {} times:".format(method_name, r.get(method_name).decode("utf-8")))
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(method_name, i.decode("utf-8"), o.decode("utf-8")))
+
+
+class Cache:
+    """Cache class"""
+
+    def __init__(self):
+        """Constructor method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis using a random key"""
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Get data from Redis"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Convert bytes to str"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Convert bytes to int"""
+        return self.get(key, int)
